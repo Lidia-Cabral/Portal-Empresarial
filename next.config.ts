dc22e0c79102d0dbb0d1e8829f3dcef3666a4a92
Lidia-Cabral <lidiacabralconsultoria@gmail.com>
2025-11-28 16:58:55 -0300

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+﻿import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Não precisa configurar srcDir, Next.js detecta automaticamente
+  reactStrictMode: true,
 };
 
 export default nextConfig;
